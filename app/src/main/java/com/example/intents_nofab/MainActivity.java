71bba8d13486ab4f7b407ed3f600dcabd7f57ea3
xkadmx <zuzanna.rosinska@yahoo.com>
2019-03-14 21:03:20 +0100

    buttonBack added to SecondActivity.java

--- app/src/main/java/com/example/intents_nofab/MainActivity.java
+++ app/src/main/java/com/example/intents_nofab/MainActivity.java
@@ -20,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // tutaj logika
+                // TODO logika
             }
-        })
+        });
     }
 }
--- app/src/main/java/com/example/intents_nofab/SecondActivity.java
+++ app/src/main/java/com/example/intents_nofab/SecondActivity.java
@@ -3,12 +3,17 @@ package com.example.intents_nofab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        buttonBack = (Button) findViewById(R.id.buttonBack);
     }
 }

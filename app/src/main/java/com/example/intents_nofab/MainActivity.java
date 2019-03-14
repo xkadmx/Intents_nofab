47cdb0cf87379647c68dd39e437e5f5f547006bc
xkadmx <zuzanna.rosinska@yahoo.com>
2019-03-14 20:53:39 +0100

    setOnClickListener dla buttonNext

--- app/src/main/java/com/example/intents_nofab/MainActivity.java
+++ app/src/main/java/com/example/intents_nofab/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.intents_nofab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonNext ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // tutaj logika
+            }
+        })
     }
 }
